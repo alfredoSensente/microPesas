6e9fba9ef87373c766fcf545c8d79b57a8ac1c75
alfredoJSensente <ajsnsn93@gmail.com>
2019-05-17 16:27:29 -0600

    pesas.ino enciende un led con pulsar un boton y lo apaga volviendolo a pulsar

--- pesas.ino
+++ pesas.ino
@@ -1,14 +1,27 @@
 #define pinled5 5
-
+#define btn_chicharra 2
+int resultado=1;
+signed long tiempo=millis();
 void setup() {
   // put your setup code here, to run once:
   pinMode(pinled5,OUTPUT);
+  pinMode(btn_chicharra,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(pinled5,HIGH);
-  delay(700);
-  digitalWrite(pinled5,LOW);
-  delay(700);
+  if (digitalRead(btn_chicharra)==HIGH and tiempo+1000<millis())
+  {
+    resultado=resultado*-1;
+  }
+  
+  if (resultado==-1)
+    {
+      digitalWrite(pinled5,HIGH);  
+    } else
+    {
+      digitalWrite(pinled5,LOW);
+    }
+    
+  
 }
