e9f64554f13273c5c6d418f9c2e1837a12ca0594
alfredoJSensente <ajsnsn93@gmail.com>
2019-05-17 14:44:24 -0600

    hago prueba en pesas.ino con un led

--- pesas.ino
+++ pesas.ino
@@ -1,9 +1,14 @@
+#define pinled5 5
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(pinled5,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(pinled5,HIGH);
+  delay(700);
+  digitalWrite(pinled5,LOW);
+  delay(700);
 }
