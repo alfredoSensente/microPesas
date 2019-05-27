a56c3afa31d4729f5ca2bda40ae2623c05ce93d3
alfredoJSensente <ajsnsn93@gmail.com>
2019-05-27 11:41:34 -0600

    agregué el metodo votar, que muestra un serialprintln que simula la chicharra y apaga los leds y chicharra despues de 5s, borré comentarios en los ifs de los botones en loop

--- pesas.ino
+++ pesas.ino
@@ -17,6 +17,8 @@
 int val1_ok = 0, val1_no = 0,val2_ok = 0, val2_no = 0,val3_ok = 0, val3_no = 0; //val se emplea para almacenar el estado del boton
 bool state1_ok= false, state1_no = false, state2_ok =false, state2_no = false, state3_ok = false, state3_no = false; // 0 LED apagado, mientras que 1 encendido
 int old_val1_ok = 0, old_val1_no = 0, old_val2_ok = 0, old_val2_no = 0, old_val3_ok = 0, old_val3_no = 0; // almacena el antiguo valor de val
+int contOk=0, contNo=0;
+bool voto1=false,voto2=false,voto3=false;
 void setup() {
   // put your setup code here, to run once:
   pinMode(luz_juez1_ok,OUTPUT);
@@ -42,54 +44,54 @@ void loop() {
     val1_ok = digitalRead(btn_juez1_ok);
     if ((val1_ok==HIGH && old_val1_ok==0) || (val1_ok==LOW && old_val1_ok==1))//
     {
-      Serial.println("valor:");
-      Serial.println(val1_ok);
-      Serial.println("old val:");
-      Serial.println(old_val1_ok);
       antirebote(val1_ok,old_val1_ok,state1_ok); 
       old_val1_ok = val1_ok;
       encenderLed(state1_ok,luz_juez1_ok);
-      apagarOtro(luz_juez1_no,state1_no,val1_no,old_val1_no); 
+      apagarOtro(luz_juez1_no,state1_no,val1_no,old_val1_no);
+      votar(state1_ok,voto1); 
     }
     //Boton NO
     val1_no = digitalRead(btn_juez1_no);
     if ((val1_no==HIGH && old_val1_no==0) || (val1_no==LOW && old_val1_no==1))//
     {
-      Serial.println("valor_no:");
-      Serial.println(val1_no);
-      Serial.println("old val_no:");
-      Serial.println(old_val1_no);
       antirebote(val1_no,old_val1_no,state1_no); 
       old_val1_no = val1_no;
       encenderLed(state1_no,luz_juez1_no);
       apagarOtro(luz_juez1_ok,state1_ok,val1_ok,old_val1_ok);  
+      votar(state1_no,voto1); 
     }
+    
   //Segundo Juez
     //Boton OK
     val2_ok = digitalRead(btn_juez2_ok);
     if ((val2_ok==HIGH && old_val2_ok==0) || (val2_ok==LOW && old_val2_ok==1))//
     {
-      Serial.println("valor:");
-      Serial.println(val2_ok);
-      Serial.println("old val:");
-      Serial.println(old_val2_ok);
       antirebote(val2_ok,old_val2_ok,state2_ok); 
       old_val2_ok = val2_ok;
       encenderLed(state2_ok,luz_juez2_ok);
       apagarOtro(luz_juez2_no,state2_no,val2_no,old_val2_no); 
+      votar(state2_ok,voto2); 
     }
     //Boton NO
     val2_no = digitalRead(btn_juez2_no);
     if ((val2_no==HIGH && old_val2_no==0) || (val2_no==LOW && old_val2_no==1))//
     {
-      Serial.println("valor_no:");
-      Serial.println(val2_no);
-      Serial.println("old val_no:");
-      Serial.println(old_val2_no);
       antirebote(val2_no,old_val2_no,state2_no); 
       old_val2_no = val2_no;
       encenderLed(state2_no,luz_juez2_no);
       apagarOtro(luz_juez2_ok,state2_ok,val2_ok,old_val2_ok);  
+      votar(state2_no,voto2); 
+    }
+    //Tercer Juez
+    //Boton OK
+    val3_ok = digitalRead(btn_juez3_ok);
+    if ((val3_ok==HIGH && old_val3_ok==0) || (val3_ok==LOW && old_val3_ok==1))//
+    {
+      antirebote(val3_ok,old_val3_ok,state3_ok); 
+      old_val3_ok = val3_ok;
+      encenderLed(state3_ok,luz_juez3_ok);
+      apagarOtro(luz_juez3_no,state3_no,val3_no,old_val3_no); 
+      votar(state3_ok,voto3); 
     }
 }
 
@@ -111,10 +113,34 @@ int encenderLed(bool estado, int led){
     digitalWrite(led,LOW);
   }
 }
-
 int apagarOtro(int led,bool &estado, int &valor, int &oldvalor){
   digitalWrite(led,LOW);
   estado=false;
   valor=0;
   oldvalor=0;
+}
+
+int votar(bool &estado,bool &voto){
+  if (voto==false)
+  {
+    if (estado==true)
+    {
+      voto=true;
+      if (voto1==true && voto2==true && voto3==true)
+      {
+        Serial.println("La chicharra suena");
+        delay(5000);
+        Serial.println("La chicharra se apaga");
+        voto1=false;
+        voto2=false;
+        voto3=false;
+        apagarOtro(luz_juez1_ok,state1_ok,val1_ok,old_val1_ok);
+        apagarOtro(luz_juez2_ok,state2_ok,val2_ok,old_val2_ok);
+        apagarOtro(luz_juez3_ok,state3_ok,val3_ok,old_val3_ok);
+        apagarOtro(luz_juez1_no,state1_no,val1_no,old_val1_no);
+        apagarOtro(luz_juez2_no,state2_no,val2_no,old_val2_no);
+        apagarOtro(luz_juez3_no,state3_no,val3_no,old_val3_no);
+      }
+    }
+  }
 }
